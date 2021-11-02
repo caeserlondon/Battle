4fcbfbaeab3d772e027b9fcab02f86c61dd2f715
Ryan Grimes <ryanpaulgrimes@gmail.com>
2021-11-02 17:44:12 +0100

    Update file to be where it was at the end of Day 2 on my pairing parters repo

---
+++ Gemfile
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+  gem "capybara"
+  gem "selenium-webdriver"
+  gem 'rack'
+  gem "thin"
+  gem "puma"
+  gem 'reel'
+  gem "http"
+  gem 'webrick'
+
+ruby '3.0.2'
+
+group :development do
+  gem "sinatra"
+  gem "sinatra-contrib"
+  gem "rspec"
+end
--- app.rb
+++ app.rb
@@ -1,29 +1,13 @@
-require 'sinatra'
-require "sinatra/reloader" if development?
+require 'sinatra/base'
+require "sinatra/reloader" 
 
-get '/' do
-  "hello!"
-end
+class Battle < Sinatra::Base
 
-get '/secret' do
-  "Can't be that secret if you're reading this"
-end
+  configure :development do
+    register Sinatra::Reloader
+  end
 
-get '/ryan' do
-  "Ryan"
-end
-
-get '/mark' do
-  "Mark"
-end
-
-get '/random-cat' do
-  @name = ["Amigo", "Misty", "Almond"].sample
-  erb(:index)
-end
-
-get '/named-cat' do
-  p params
-  @name = params[:name]
-  erb(:index)
+  get '/' do
+    'Testing infrastructure working!'
+  end
 end
