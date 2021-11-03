2b25ba022fd1ba4b112f1ef78411293b96f83c56
Ryan Grimes <ryanpaulgrimes@gmail.com>
2021-11-03 13:36:48 +0100

    Implement POST/redirect/GET pattern

--- app.rb
+++ app.rb
@@ -2,18 +2,25 @@ require 'sinatra/base'
 require "sinatra/reloader" 
 
 class Battle < Sinatra::Base
-
+  enable :sessions
   configure :development do
     register Sinatra::Reloader
   end
 
+ 
   get '/' do
     erb :index
   end
 
   post '/names' do
-    @player_one_name = params[:player_one_name]
-    @player_two_name = params[:player_two_name]
+    session[:player_one_name] = params[:player_one_name]
+    session[:player_two_name] = params[:player_two_name]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player_one_name = session[:player_one_name]
+    @player_two_name = session[:player_two_name]
     erb :play
   end
 end
